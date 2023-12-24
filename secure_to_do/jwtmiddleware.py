77c123c1a1677047bda492c24ba6e534bb7d5e60
--replace-all <m2017info@gmail.com>
2023-12-24 20:23:46 +0300

    with middleware for socket security

--- secure_to_do/jwtmiddleware.py
+++ secure_to_do/jwtmiddleware.py
@@ -1,40 +1,35 @@
-import jwt
 from channels.db import database_sync_to_async
-from channels.middleware import BaseMiddleware
+from rest_framework_simplejwt.tokens import AccessToken
 from django.contrib.auth import get_user_model
+from channels.middleware import BaseMiddleware
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from channels.exceptions import DenyConnection
 
 User = get_user_model()
-
 
 @database_sync_to_async
 def get_user(user_id):
     try:
-        return User.objects.get(pk=user_id)
+        return User.objects.get(id=user_id)
     except User.DoesNotExist:
         return None
 
-
-class JWTAuthMiddleware(BaseMiddleware):
+class TokenAuthMiddleware(BaseMiddleware):
     def __init__(self, inner):
-        super().__init__(inner)
         self.inner = inner
+        self.jwt_auth = JWTAuthentication()
 
     async def __call__(self, scope, receive, send):
-        headers = dict(scope["headers"])
-        if b"authorization" in headers:
-            try:
-                auth_header = headers[b"authorization"].decode()
-                token_prefix, token = auth_header.split(" ")
-                if token_prefix.lower() == "bearer":
-                    decoded_token = jwt.decode(
-                        token,
-                        options={"verify_signature": True},
-                        algorithms=["HS256"],
-                    )
-                    user_id = decoded_token["user_id"]
-                    scope["user"] = await get_user(user_id)
-            except (jwt.DecodeError, jwt.InvalidTokenError, KeyError, User.DoesNotExist) as e:
-                # Handle exceptions or log the error
-                pass
+        token = scope['query_string'].decode().split('=')[1]
+        try:
+            access_token = AccessToken(token)
+            user = await get_user(access_token['user_id'])
+            if user is not None:
+                scope['user'] = user
+                scope['user_id'] = user.id
+            else:
+                raise DenyConnection("Invalid token or user not found.")
+        except Exception as e:
+            raise DenyConnection("Invalid token or user not found.")
 
-        return await super().__call__(scope, receive, send)
+        return await self.inner(scope, receive, send)
